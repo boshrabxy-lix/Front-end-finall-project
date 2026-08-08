678c2728070f93d6eae452020ad5372ff39f203e
boshrabxy-lix <boshrabxy@gmail.com>
2026-08-08 20:20:34 +0300

    fix ProfileProdviewer import2

--- src/pages/profile/ProfileInfo.jsx
+++ src/pages/profile/ProfileInfo.jsx
@@ -1,15 +1,34 @@
-import React from 'react'
 import ProfileDetails from './ProfileDetails'
-import { Box } from '@mui/material';
-
 import ProfileProdviewer from './ProfileProdviewer';
-
+import React from 'react'
+import useProducts from '../../hooks/useProducts'
+import Loader from '../../components/loader/Loader';
+import { Box, Grid, Typography } from '@mui/material';
+import Product from '../../ui/product/ProductProfileUi';
+import getTheme from "../../Theme";
+import { useTranslation } from 'react-i18next';
 
 export default function ProfileInfo() {
+ const { t } = useTranslation();
+    const { data, isError, isLoading, error } = useProducts(2);
+
+    if (isLoading) return <Loader />
+    if (isError) return <Box color={'red'}>{error.message}</Box>
   return (
     <Box>
       <ProfileDetails />
-      <ProfileProdviewer />
+
+
+      <Box sx={{py:3}}>
+            <Typography component={'h2'} variant='h4' gutterBottom sx={{ color: "primary", fontSize: 38, fontWeight: 700, mt: 3 }}>{t('Picked For You')}</Typography>
+            <Grid container spacing={1}>
+                {data.response.data.map(product =>
+                    <Grid item size={{ xs: 12, sm: 4 }} key={product.id} >
+                        <Product product={product} />
+                    </Grid>
+                )}
+            </Grid>
+        </Box>
     </Box>
   )
 }
